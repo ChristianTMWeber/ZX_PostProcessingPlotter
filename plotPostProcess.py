bc113273aa581e7f381f417d2030cd5bf244cd55
Christian Weber <Christian.Weber@yale.edu>
2018-05-29 23:25:29 -0400

    Feature: Added helper class 'DSIDHelper' to help with sorting DSIDs into bins of Physics processes, associate DSID with more detailled descriptions of the samples. All in preparation to bin plot samples into physics processes. Moved the metadata infor gathering and provision also to there

--- plotPostProcess.py
+++ plotPostProcess.py
@@ -11,6 +11,104 @@ import numpy as np # good ol' numpy
 import warnings # to warn about things that might not have gone right
 import itertools # to cycle over lists in a nice automated way
 import re # to do regular expression matching
+#import collections # so we can use collections.defaultdict to more easily construct nested dicts on the fly
+
+
+class DSIDHelper:
+
+
+    physicsProcess={"H->ZZ*->4l" : [341947, 345060, 341488, 345046, 345047, 345048, 345066, 344973, 344974],
+                      "ZZ*->4l" :    [364250, 364251, 364252, 361603, 342556, 343232, 343212, 343213],
+                      "Reducible (Z+Jets, WZ, ttbar)"  : [364114, 364115, 364116, 364117, 364118, 364119, 364120, 364121, 364122, 
+                                                          364123, 364124, 364125, 364126, 364127, 364100, 364101, 364102, 364103, 
+                                                          364104, 364105, 364106, 364107, 364108, 364109, 364110, 364111, 364112, 
+                                                          364113, 364128, 364129, 364130, 364131, 364132, 364133, 364134, 364135, 
+                                                          364136, 364137, 364138, 364139, 364140, 364141, 361601, 410472],
+                       "VVV/VBS" : [364248, 364247, 364245, 364243, 364364],
+                       "Z+(ttbar/J/Psi/Upsilon)" : [410142] 
+                    }
+
+    physicsSubProcess = {"ggH" : [345060], "VBFH":[341488], "WH" : [341964], "ZH" : [341947],
+                     "ggZH" : [345066], "ttH125" : [345046, 345047, 345048], "bbH" : [344973, 344974],
+                     "qq->ZZ*->4l" : [364250, 364251, 364252], "gg->ZZ*->4l" : [345708, 345709],
+                     "ZZZ" : [364248, 364247], "WZZ" : [364245], "WWZ" : [364243], 
+                     "lllljj" : [364364], "ttll" : [410142], "WZ" : [361601],
+                     "Z+Jets (Sherpa)" : [364114, 364115, 364116, 364117, 364118, 364119, 364120, 
+                                          364121, 364122, 364123, 364124, 364125, 364126, 364127, 
+                                          364100, 364101, 364102, 364103, 364104, 364105, 364106, 
+                                          364107, 364108, 364109, 364110, 364111, 364112, 364113, 
+                                          364128, 364129, 364130, 364131, 364132, 364133, 364134, 
+                                          364135, 364136, 364137, 364138, 364139, 364140, 364141]
+                     }
+
+    def __init__(self):
+        self.physicsProcessByDSID    = self.makeReverseDict( self.physicsProcess);
+        self.physicsSubProcessByDSID = self.makeReverseDict( self.physicsSubProcess);
+
+
+
+
+    def getProduct_CrossSec_kFactor_genFiltEff(self, DSID):
+        DSID = int(DSID)
+        
+        prod = self.metaDataDict[DSID]["crossSection"] * self.metaDataDict[DSID]["kFactor"] * self.metaDataDict[DSID]["genFiltEff"]
+        
+        return prod
+
+
+        
+    
+    def makeReverseDict(self, inputDict):
+        # Lets say we have a dict that goes like:
+        # dict = { "key1" : [1, 2, 3], "key2" : [4,5], "key3" : 7}
+        # this function makes a 'reverse dict' that goes like
+        # reverseDict = {1 : "key1", 2 : "key1", 3 : "key1", 4 : "key2", 5 : "key2", 7 : "key3"}
+
+        reverseDict = {}
+
+        for key in inputDict.keys():
+        # distinguish between lists / ntuples and things that are not that easily iterable
+            if isinstance(inputDict[key], (list, tuple)):
+                 for element in inputDict[key]:
+                    if element in reverseDict: warnings.warn("Non unique reverse mapping in dict[key] -> key")
+                    reverseDict[element] = key
+            else: 
+                if element in reverseDict: warnings.warn("Non unique reverse mapping in dict[key] -> key")
+                reverseDict[inputDict[key]] = key
+        return reverseDict
+
+
+    def importMetaData(self,metadataFileLocation):
+        # parse the metada data from a metadata text file that we furnish
+        # we expect the metadata file to have the stucture:
+        # <DSID> <crossSection> <kFactor> <genFiltEff>   <...>
+        # There the different values are seperate by whitespace
+        # We ignore lines that do not start with a DSID (i.e. a 6 digit number)
+
+        metaDataDict = {}
+        physicsShort = {}
+
+        DSIDPattern = re.compile("\d{6}") # DSIDs are numbers 6 digits long
+
+        metadataFile = open(metadataFileLocation,'r')
+
+        for line in metadataFile:
+            if re.match(DSIDPattern, line): #if the line starts with out pattern
+                splittedLine = line.split() # split at whitespace
+                DSID = int(splittedLine[0])
+                tempDict = { "crossSection" : float(splittedLine[1]),  
+                             "kFactor"      : float(splittedLine[2]),
+                             "genFiltEff"   : float(splittedLine[3])}
+
+                metaDataDict[DSID] = tempDict
+                physicsShort[DSID] = splittedLine[4]
+
+        self.metaDataDict=metaDataDict
+        self.physicsShort=physicsShort
+        return metaDataDict, physicsShort
+
+
+
 
 
 def getTDirsAndContents(TDir, outputDict = {}, recursiveCounter = 0):
@@ -90,29 +188,7 @@ def setupTLegend():
 
     return TLegend
 
-def importMetaData(metadataFileLocation):
-    # parse the metada data from a metadata text file that we furnish
-    # we expect the metadata file to have the stucture:
-    # <DSID> <crossSection> <kFactor> <genFiltEff>   <...>
-    # There the different values are seperate by whitespace
-    # We ignore lines that do not start with a DSID (i.e. a 6 digit number)
 
-    metaDataDict = {}
-    DSIDPattern = re.compile("\d{6}") # DSIDs are numbers 6 digits long
-
-    metadataFile = open(metadataFileLocation,'r')
-
-    for line in metadataFile:
-        if re.match(DSIDPattern, line): #if the line starts with out pattern
-            splittedLine = line.split() # split at whitespace
-            DSID = int(splittedLine[0])
-            tempDict = { "crossSection" : float(splittedLine[1]),  
-                         "kFactor"      : float(splittedLine[2]),
-                         "genFiltEff"   : float(splittedLine[3])}
-
-            metaDataDict[DSID] = tempDict
-
-    return metaDataDict
 
 def getSumOfWeigts(topLevelTObjects):
     # get the sum of weights for our MC samples
@@ -149,12 +225,17 @@ if __name__ == '__main__':
     lumiMap= { "mc16a" : 36.1029, "mc16d" : 43.5382, "units" : "fb-1"}  # campaigns integrated luminosity,  complete + partial
 
 
+    mac16aDISDHelper = DSIDHelper()
+
+    mac16aDISDHelper.importMetaData(bkgMetaFilePaths["mc16a"])
+
+    import pdb; pdb.set_trace()
+
+    #metdataMC16a = importMetaData(bkgMetaFilePaths["mc16a"])
 
 
-    metdataMC16a = importMetaData(bkgMetaFilePaths["mc16a"])
-
-
-    inputRootFileName = "data15_mc16a.root"
+    #inputRootFileName = "ZX_data15_mc16a.root"
+    inputRootFileName = "ZdZd_SRHM_data1516_mc16a.root"
 
     postProcessedData = ROOT.TFile(inputRootFileName,"READ");
 
@@ -185,7 +266,7 @@ if __name__ == '__main__':
 
             dataTH1 = None 
 
-            backgroundTHStack = ROOT.THStack(histEnding,histEnding)
+            
             
 
             # define fill colors, use itertools to cycle through them, access via fillColors.next()
@@ -195,26 +276,44 @@ if __name__ == '__main__':
 
 
 
+            
+            #histEnding = "ZXSR_HiggsMassVeto_M4l"
+            #histEnding = "SRHM_Final_M4l"
+            #histEnding = "SRHM_Final_avgMll"
+            histEnding = "VR1HM_Final_avgMll"
 
-
-
+            backgroundTHStack = ROOT.THStack(histEnding,histEnding)
+            backgroundTHStack.SetMaximum(25.)
             canvas = ROOT.TCanvas(histEnding,histEnding,1280,720);
             legend = setupTLegend()
+
+
 
             for histName in histsByEnding[histEnding]: 
                 print(histName)
                 DSID = histName.split("_")[1]
                 currentTH1 = TDir.Get(histName)
-
+                currentTH1.GetYaxis().SetRange(0,25);
 
                 
                 if int(DSID) > 0: # Signal & Background have DSID > 0
                     currentTH1.SetFillStyle(1001) # 1001 - Solid Fill: https://root.cern.ch/doc/v608/classTAttFill.html
                     currentTH1.SetFillColor(fillColors.next())
 
-                    
-                    scale = lumiMap[campaign] * 1000000. * metdataMC16a[int(DSID)]["crossSection"] * metdataMC16a[int(DSID)]["kFactor"] * metdataMC16a[int(DSID)]["genFiltEff"] / sumOfWeights[int(DSID)]
+
+
+                    scale = lumiMap[campaign] * 1000000. * mac16aDISDHelper.getProduct_CrossSec_kFactor_genFiltEff(DSID) / sumOfWeights[int(DSID)]
+
+                    print( DSID, currentTH1.Integral(), scale, currentTH1.Integral()*scale)
                     currentTH1.Scale(scale)
+
+                    #if int(DSID) == 345047:
+                    #    currentTH1.Draw()
+                    #    canvas.Update()
+                    #    import pdb; pdb.set_trace()
+
+                    #if currentTH1.Integral() < 1. : continue
+
                     backgroundTHStack.Add(currentTH1) 
                     legend.AddEntry(currentTH1 ,histName, "f");
                 else:   # data has DSID 0 for us  
@@ -227,8 +326,8 @@ if __name__ == '__main__':
 
             backgroundTHStack.Draw("Hist")
             dataTH1.Draw("same")
+            backgroundTHStack.GetYaxis().SetRange(0,25);
             legend.Draw();
-
 
             canvas.Update() # we need to update the canvas, so that changes to it (like the drawing of a legend get reflected in its status)
             import pdb; pdb.set_trace()
