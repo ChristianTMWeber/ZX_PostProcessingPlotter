0d067ccb84145d3395416d11dd05dc19ed9986bc
Christian Weber <Christian.Weber@yale.edu>
2019-04-18 15:51:12 -0400

    Refactoring code, for conciceness and in preparation for calculating uncertainties on expected significance

--- m4lRangeStudy.py
+++ m4lRangeStudy.py
@@ -13,6 +13,7 @@ import resource # print 'Memory usage: %s (kb)' % resource.getrusage(resource.RU
 import plotPostProcess as postProcess
 from functions.histTools import getSmallestInterval # my own tools for ROOT histograms 
 import math # for simple math
+import time # for measuring execution time
 
 
 def defineTargetHistograms(lowerLimitList, upperLimitList, templateHist = ROOT.TH1D( "templateHist", "TemplateHist", 150,0,150) ):
@@ -35,12 +36,39 @@ def defineTargetHistograms(lowerLimitList, upperLimitList, templateHist = ROOT.T
     return th1dDict
 
 
-def defineSetOfM4lFilteredHists( RDF, lowerLimitList, upperLimitList, myTH1DModel = None , weightVariable = None):
+def returnFilteredHist( myRDataFrame, targetVariable , myTH1DModel = None , weightVariable = None ):
+
+    if    myTH1DModel is not None and weightVariable is not None : RDFResultHisto=myRDataFrame.Histo1D(myTH1DModel, targetVariable, weightVariable)
+    elif  myTH1DModel is not None and weightVariable is     None : RDFResultHisto=myRDataFrame.Histo1D(myTH1DModel, targetVariable)
+    elif  myTH1DModel is     None and weightVariable is not None : RDFResultHisto=myRDataFrame.Histo1D(targetVariable, weightVariable)
+    else:                                                          RDFResultHisto=myRDataFrame.Histo1D(targetVariable)
+
+    return RDFResultHisto
+
+
+
+def defineSetOfM4lFilteredHists( RDF, lowerLimitList, upperLimitList, myTH1DModel = None , weightVariable = None, targetVariable = "m34", makeComplimentaryHists = False):
     # RDF is expected to be a OOT.RDataFrame
     # lowerLimitList and upperLimitList  don't really need to be lists, just iterables
+    # makeComplimentaryHists - if true, we will figure out the widest of all the possible limits on m4l, 
+    #                          and then for any pair of limits in (m4lBelow, m4lAbove), we will fill another histogram 
+    #                          with the events that do not make it into the current set of limits, but would make it into the one with the widest set of possible limits
+
+    # subfinction to help me deal with the different cases of myTH1DModel and weightVariable being provided or not
+    def returnFilteredHist( myRDataFrame, targetVariable , myTH1DModel = None , weightVariable = None ):
+
+        if    myTH1DModel is not None and weightVariable is not None : RDFResultHisto=myRDataFrame.Histo1D(myTH1DModel, targetVariable, weightVariable)
+        elif  myTH1DModel is not None and weightVariable is     None : RDFResultHisto=myRDataFrame.Histo1D(myTH1DModel, targetVariable)
+        elif  myTH1DModel is     None and weightVariable is not None : RDFResultHisto=myRDataFrame.Histo1D(targetVariable, weightVariable)
+        else:                                                          RDFResultHisto=myRDataFrame.Histo1D(targetVariable)
+
+        return RDFResultHisto
+
 
 
     histoDict = {} # store my RDF.Histo1D s here as { (m4lLowLimit, m4lUpperLimit) : RDF.Histo1D, ... }
+    histoDictComplimenatry = {}
+
 
     for m4lAbove in lowerLimitList:
         for m4lBelow in upperLimitList:
@@ -50,15 +78,26 @@ def defineSetOfM4lFilteredHists( RDF, lowerLimitList, upperLimitList, myTH1DMode
 
             filteredRDF = RDF.Filter( m4lLowLimit ).Filter( m4lUpperLimit )
 
-            if    myTH1DModel is not None and weightVariable is not None : RDFResultHisto=filteredRDF.Histo1D(myTH1DModel, "m34", weightVariable)
-            elif  myTH1DModel is not None and weightVariable is     None : RDFResultHisto=filteredRDF.Histo1D(myTH1DModel, "m34")
-            elif  myTH1DModel is     None and weightVariable is not None : RDFResultHisto=filteredRDF.Histo1D("m34", weightVariable)
-            else:                                                          RDFResultHisto=filteredRDF.Histo1D("m34")
+            RDFResultHisto = returnFilteredHist( filteredRDF ,targetVariable , myTH1DModel = myTH1DModel , weightVariable = weightVariable )
 
             histoDict[ (m4lAbove,m4lBelow)] = RDFResultHisto
+
+            if makeComplimentaryHists:
+                # we want events whose m4l is either of two non-overlapping intervals: m4l \in (A,B) or m4l \in (C,D), with A<B<C<D, let's define those
+                #                                A                  B           C                       D
+                leftAndRightIntervals  = (min(lowerLimitList), m4lLowLimit, m4lUpperLimit      , max(upperLimitList) )
+                cutString = "( m4l > %i $$ m4l < %i ) || m4l > %i $$ m4l < %i )" %( leftAndRightIntervals )
+
+                filteredRDFComplimentary = RDF.Filter( cutString )
+
+                histoDictComplimenatry[ (m4lAbove,m4lBelow) ]  = returnFilteredHist( filteredRDFComplimentary ,targetVariable , myTH1DModel = myTH1DModel , weightVariable = weightVariable, makeComplimentaryHists = False )
+                
+
+
+
     
     #import pdb; pdb.set_trace() # import the debugger and instruct it to stop here
-    return histoDict
+    return histoDict, histoDictComplimenatry
 
 def addToTargetHists(targetHistDict, sourceHistDict, scale = 1.):
 
@@ -124,6 +163,26 @@ def histByHistGetIntegralsAndApplyArithmetics(histDict1, histDict2, arithmetic =
 
     return outputDict
 
+def doArithmeticOnQualifiedHistIntegrals(hist1, hist2, arithmetic = None , desiredWidth = 0.99 ):
+    # pass a function that takes two floats and returns one as 'arithmetic' parameters,
+    # the two inputs weil will be the ratio of the integrals from histDict1[ givenKey ] and histDict2[ givenKey ] 
+    # where the integral boulds are given by the smallest integral in histDict1[ givenKey ] that subtents 95% of the events in there
+    # for example if want want to take the ratio choose
+    if arithmetic is None: arithmetic = lambda A, B : A/B
+
+    xLow, xHigh = getSmallestInterval( hist1, desiredWidth = desiredWidth ) # get the smallest intervall that subtends a fraction 'desiredWidth' of all events
+
+    # let's practice lambda functions, and define one that gives me, for a given histogram 'aHist' and an 'xValue', the bin number for the hist of interest
+    # the syntax is the following: 
+    #  <name of the function>  =   lambda(as sign for python that we get an inline function here)   <parameters of the function>  :   <definition of the function?
+    getBinNr = lambda aHist, xValue : aHist.GetXaxis().FindBin(xValue)
+
+    integral1 = hist1.Integral( getBinNr(hist1, xLow),getBinNr(hist1, xHigh) )
+    integral2 = hist2.Integral( getBinNr(hist2, xLow),getBinNr(hist2, xHigh) )
+
+    return arithmetic(integral1,integral2)
+
+
 
 
 def writeTH2AndGetCanvas( TH2 ):
@@ -137,6 +196,22 @@ def writeTH2AndGetCanvas( TH2 ):
 
     return tmpCanvas
 
+
+def makeResultsTH2( signalHistDict , backgroundHistDict, titleString, signalBackgroundComparisonOperation ):
+
+    currentSignalTH2 = myTH2Template.Clone( titleString )
+    currentSignalTH2.SetTitle(titleString)
+
+    for m4lLowLimit, m4lUpperLimit in signalHistDict:
+
+        signalHist = signalHistDict[(m4lLowLimit, m4lUpperLimit)]
+        backgroundHist = backgroundHistDict[(m4lLowLimit, m4lUpperLimit)]
+
+        aNumber = signalBackgroundComparisonOperation(signalHist , backgroundHist) 
+
+        currentSignalTH2.Fill(m4lLowLimit, m4lUpperLimit, aNumber )
+
+    return currentSignalTH2
 
 
 if __name__ == '__main__':
@@ -200,11 +275,10 @@ if __name__ == '__main__':
     myDSIDHelper.fillSumOfEventWeightsDict(args.input)
 
     ######################################################
-    #Select relevant (ROOT) objects
+    # Prepare some datastructes and run parameters
     ######################################################
 
-    postProcessedData = ROOT.TFile(args.input, "READ"); # open the file with te data from the ZdZdPostProcessing
-
+    # set of m4l ranges we want to consider for our study
     m4lRangeLow = range(115,125,1); m4lRangeHigh = range(125,131,1)
     dX = float(m4lRangeLow[1] - m4lRangeLow[0])
     dY = float(m4lRangeHigh[1] - m4lRangeHigh[0])
@@ -214,9 +288,14 @@ if __name__ == '__main__':
     targetHistTemplate.GetXaxis().SetTitle("m34 [GeV]")
 
 
+    # target hists: a dict of TH1Ds mapping the tuple (m4lLow, m4lHigh) -> TH1D for that tuple, for each pair of (m4lLow, m4lHigh) that we consider 
     targetHistsBackground = defineTargetHistograms(m4lRangeLow, m4lRangeHigh, templateHist = targetHistTemplate)
-    targetHistsSignal     = defineTargetHistograms(m4lRangeLow, m4lRangeHigh, templateHist = targetHistTemplate)
-    dictOfSignalTargetHists = {}
+    dictOfSignalTargetHists = {} # will contain a mappint of DSID -> targetHistsSignal for all signal DSIDs
+
+    # complimentary hists contain the m34 distribution of events for which  min(m4lRangeLow) < m4l < m4lLow or m4lHigh < m4l < max(m4lRangeHigh)
+    # we will use them in the calculation of some errors eventually
+    complimentaryHistsBackground = defineTargetHistograms(m4lRangeLow, m4lRangeHigh, templateHist = targetHistTemplate)
+    dictOfSignalComplementaryHists = {} # will contain a mappint of DSID -> targetHistsSignal for all signal DSIDs
 
     # define this as a model for the RDataFrome histograms
     myTH1DModel = ROOT.RDF.TH1DModel(targetHistTemplate)
@@ -226,7 +305,14 @@ if __name__ == '__main__':
     myTH2Template.GetYaxis().SetTitle("upper limit on m4l")
     myTH2Template.SetStats( False) # remove stats box
 
+    ######################################################
+    # Do TTree selection an filtering via RDataFrame
+    ######################################################
+
+    postProcessedData = ROOT.TFile(args.input, "READ"); # open the file with the data from the ZdZdPostProcessing, that contains the relevant TTrees
+
     ROOT.ROOT.EnableImplicitMT()
+    startTime = time.time()
     # loop over all of the TObjects in the given ROOT file
     for path, myTObject  in postProcess.generateTDirPathAndContentsRecursive(postProcessedData, newOwnership = None):  
         # set newOwnership to 'None' here and let root handle the ownership itself for now, 
@@ -234,7 +320,7 @@ if __name__ == '__main__':
 
         if postProcess.irrelevantTObject(path, myTObject, requiredRootType=ROOT.TTree): continue # skip non-relevant histograms
         
-        print path + "\t Memory usage: %s (kB)" % (resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/8)
+        
 
         # path happen to include also the rootfile name, e.g. 'testDir.root/345060/Nominal/t_ZXTree'
         # we need to remove that part of the string, so select everything after the first "/"
@@ -246,17 +332,28 @@ if __name__ == '__main__':
         RDFrameVariables = myDataFrame.Define("m34","llll_m34 / 1000").Define("m4l","llll_m4l / 1000") # define our variables
 
         # we want to have multiple m4l filtered histograms. Let's define them here
-        RDFHistDict = defineSetOfM4lFilteredHists( RDFrameVariables, m4lRangeLow, m4lRangeHigh , myTH1DModel = myTH1DModel,  weightVariable = 'weight')
+        RDFHistDict, RDFHistDictComplimentary  = defineSetOfM4lFilteredHists( RDFrameVariables, m4lRangeLow, m4lRangeHigh , myTH1DModel = myTH1DModel,  weightVariable = 'weight', targetVariable = "m34")
 
         # get the DSID to decide whether the given histogram is signal or background
         DSID = postProcess.idDSID(path)
 
         if myDSIDHelper.isSignalSample( int(DSID) ):
-            if DSID not in dictOfSignalTargetHists:  dictOfSignalTargetHists[DSID] = defineTargetHistograms(m4lRangeLow, m4lRangeHigh, templateHist = targetHistTemplate)
-            currentTarget = dictOfSignalTargetHists[DSID]
-        else :  currentTarget = targetHistsBackground
+            
+            if DSID not in dictOfSignalTargetHists:  # if the current sample is a signal one, make sure we have it in our dictOfSignalTargetHists 
+                dictOfSignalTargetHists[DSID] = defineTargetHistograms(m4lRangeLow, m4lRangeHigh, templateHist = targetHistTemplate)
+                dictOfSignalComplementaryHists[DSID] = defineTargetHistograms(m4lRangeLow, m4lRangeHigh, templateHist = targetHistTemplate)
 
-        addToTargetHists(currentTarget, RDFHistDict, scale = myDSIDHelper.getMCScale(DSID) )
+            currentTarget = dictOfSignalTargetHists[DSID]
+            currentComplementaryTarget = dictOfSignalComplementaryHists[DSID]
+
+        else :
+            currentTarget = targetHistsBackground
+            currentComplementaryTarget = complimentaryHistsBackground 
+
+        addToTargetHists(currentTarget             , RDFHistDict             , scale = myDSIDHelper.getMCScale(DSID) )
+        addToTargetHists(currentComplementaryTarget, RDFHistDictComplimentary, scale = myDSIDHelper.getMCScale(DSID) )
+
+        print path + "\t Memory usage: %s kB \t Runtime: %10.1f s" % (resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/8, (time.time() - startTime ) )
 
 
         # targetHistsBackground.values()[0].Integral()
@@ -284,56 +381,34 @@ if __name__ == '__main__':
 
     fillTH2WithTargetHists( backgroundTH2, targetHistsBackground)
 
-    # signal
 
     signalOverviewDict = {}
-    for DSID in dictOfSignalTargetHists:
-        signalTitleString = "#signal events in ZX m34 signal region: "
-        signalTitleString += myDSIDHelper.physicsProcessSignalByDSID[ int(DSID) ]
-
-        currentSignalTH2 = myTH2Template.Clone( signalTitleString )
-        currentSignalTH2.SetTitle(signalTitleString)
-        fillTH2WithTargetHists( currentSignalTH2, dictOfSignalTargetHists[DSID] )
-
-
-        signalOverviewDict[DSID]=currentSignalTH2
-
-
-
-    # make TH2 overview plots of the signal / background and  significance = signal / sqrt( signal + background)
-    # in each case we are looking in the region where we find 95% of the signal
-
     ratioTH2Dict = {}  
-    for DSID in dictOfSignalTargetHists:
-        ratioTitleString = "#signal/#background ratio in ZX m34 signal region: "
-        ratioTitleString += myDSIDHelper.physicsProcessSignalByDSID[ int(DSID) ]
-
-        currentSignalTH1Dict = dictOfSignalTargetHists[DSID]
-
-        getRatioDict = histByHistGetIntegralsAndApplyArithmetics(currentSignalTH1Dict, targetHistsBackground,  arithmetic = lambda A, B : A/B)
-
-        currentSignalTH2 = myTH2Template.Clone( ratioTitleString )
-        currentSignalTH2.SetTitle(ratioTitleString)
-        fillTH2WithTargetHists( currentSignalTH2, getRatioDict  )
-
-        ratioTH2Dict[DSID] = currentSignalTH2
-
-
     significanceTH2Dict = {}
 
     for DSID in dictOfSignalTargetHists:
-        significanceTitleString = "signal significance in ZX m34 signal region: "
-        significanceTitleString += myDSIDHelper.physicsProcessSignalByDSID[ int(DSID) ]
 
-        currentSignalTH1Dict = dictOfSignalTargetHists[DSID]
+        currentSignalSampleName = myDSIDHelper.physicsProcessSignalByDSID[ int(DSID) ]
 
-        getSignificanceDict = histByHistGetIntegralsAndApplyArithmetics(currentSignalTH1Dict, targetHistsBackground,  arithmetic = lambda A, B : A/math.sqrt(A+B) ) 
+        #### Plot just the number of signal events
 
-        currentSignalTH2 = myTH2Template.Clone( significanceTitleString )
-        currentSignalTH2.SetTitle(significanceTitleString)
-        fillTH2WithTargetHists( currentSignalTH2, getSignificanceDict  )
+        signalTitleString = "#signal events in ZX m34 signal region: " + currentSignalSampleName
+        justTheSignalIntegral = lambda signalHist, backgroundHist : signalHist.Integral()
+        signalOverviewDict[DSID] = makeResultsTH2( dictOfSignalTargetHists[DSID] , targetHistsBackground, signalTitleString, justTheSignalIntegral )
 
-        significanceTH2Dict[DSID] = currentSignalTH2
+        #### Plot signal to background ratio
+        ratioTitleString = "#signal/#background ratio in ZX m34 signal region: "  + currentSignalSampleName
+        getRatio = lambda signalHist, backgroundHist : doArithmeticOnQualifiedHistIntegrals(signalHist, backgroundHist ,  arithmetic = lambda A, B : A/B)
+        ratioTH2Dict[DSID] = makeResultsTH2( dictOfSignalTargetHists[DSID] , targetHistsBackground, ratioTitleString, getRatio )
+
+
+        #### Plot the expected significance
+        significanceTitleString = "signal significance in ZX m34 signal region: "  + currentSignalSampleName
+        getSignificance = lambda signalHist, backgroundHist : doArithmeticOnQualifiedHistIntegrals(signalHist, backgroundHist ,  arithmetic = lambda A, B : A/math.sqrt(A+B) )
+
+        significanceTH2Dict[DSID] = makeResultsTH2( dictOfSignalTargetHists[DSID] , targetHistsBackground, significanceTitleString, getSignificance )
+
+
 
 
     outputFile = ROOT.TFile("m4lStudyOut.ROOT","RECREATE")
