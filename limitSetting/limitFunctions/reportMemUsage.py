d76174399f3d6ae03caf223dc41697391a8674d1
Christian Weber <ChristianTMWeber@gmail.com>
2019-09-27 14:35:43 -0400

    Bugfix: reportMemUsage was underreporting the memory usage by a factor 8

--- limitSetting/limitFunctions/reportMemUsage.py
+++ limitSetting/limitFunctions/reportMemUsage.py
@@ -5,7 +5,8 @@ import datetime # to convert seconds to hours:minutes:seconds
 
 def reportMemUsage(startTime = None):
 
-    displayString = "Memory usage: %s kB \t Runtime: " % (resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/8) 
+	# ru_maxrss is actually in kilobytes: http://man7.org/linux/man-pages/man2/getrusage.2.html
+    displayString = "Memory usage: %s kB \t Runtime: " % (resource.getrusage(resource.RUSAGE_SELF).ru_maxrss) 
 
     if startTime is not None: displayString += str(datetime.timedelta(seconds=( time.time()- startTime) ))
     
