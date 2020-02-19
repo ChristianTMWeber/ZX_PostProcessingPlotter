720e7a4121103ed6ee349db5bf9ca39c12cd0512
Christian Weber <ChristianTMWeber@gmail.com>
2020-02-19 18:15:37 -0500

    Rafactored check_H4l_FSR_constraint_impact.py heavily

--- limitSetting/reducibleChecks/check_H4l_FSR_constraint_impact.py
+++ limitSetting/reducibleChecks/check_H4l_FSR_constraint_impact.py
@@ -4,6 +4,8 @@
 
 import ROOT
 
+import re
+
 import sys 
 from os import path
 sys.path.append( path.dirname( path.dirname( path.dirname( path.abspath(__file__) ) ) ) ) # need to append the parent directory here explicitly to be able to import files from parent directories
@@ -23,62 +25,96 @@ def setupTLegend():
     return TLegend
 
 
-if __name__ == '__main__':
+def fillMZ2HistPair(aTTree, uncorrectedHist, ZConstrFSRHist, cutOn = ""):
 
-    #miniTreeFile = ROOT.TFile("data15to16_13TeV.root","OPEN")
-    miniTreeFile = ROOT.TFile("mc16_13TeV.345706.Sherpa_222_NNPDF30NNLO_ggllll_130M4l.root","OPEN")
+    aTTree.Draw("mZ2_unconstrained >>" + uncorrectedHist.GetName(), cutOn)
+    aTTree.Draw("mZ2_constrained   >>" + ZConstrFSRHist.GetName(), cutOn)
 
+    return None
 
-    miniTree = miniTreeFile.Get("tree_incl_all")
+def getDSIDStr(sampleName): return re.search("\d{6}",sampleName ).group()
 
-    referenceHist = ROOT.TH1D("referenceHist", "referenceHist", 100,0,10)
-    referenceHist.SetCanExtend(ROOT.TH1.kAllAxes)
+def prepHistograms(miniTreeName):
 
-    m34_FSR_Constrained = referenceHist.Clone("m34_FSR_Constrained")
+    if isinstance(miniTreeName, ROOT.TObject): miniTreeName = miniTreeName.GetName()
 
-    cutOn = "m4l_constrained >115 && m4l_constrained<130"
+    DSID = getDSIDStr(miniTreeName)
 
-    miniTree.Draw("mZ2_unconstrained>>referenceHist", cutOn)
-    miniTree.Draw("mZ2_constrained>>m34_FSR_Constrained", cutOn)
+    uncorrectedHist = ROOT.TH1D("uncorrectedHist_"+DSID, "uncorrectedHist_"+DSID, 100,0,10)
+    uncorrectedHist.SetCanExtend(ROOT.TH1.kAllAxes)
 
-    #aHist = miniTree.Draw("mZ1_unconstrained")
+    ZConstrFSRHist = uncorrectedHist.Clone("ZConstrFSRHist_"+DSID)
 
+    uncorrectedHist_m4lAll = uncorrectedHist.Clone("uncorrectedHist_"+DSID +"_m4lAll")
+    ZConstrFSRHist_m4lAll = uncorrectedHist.Clone("ZConstrFSRHist_"+DSID +"_m4lAll")
 
-    #referenceHist.SetTitle("full m4l range")
-    referenceHist.SetTitle("m4l in Higgs Window")
-
-
-    #referenceHist.SetFillStyle(3244)
-    referenceHist.SetMarkerColor(1)
-    referenceHist.GetYaxis().SetTitle("Events / " + str(referenceHist.GetBinWidth(1) )+" GeV" )
-    referenceHist.GetYaxis().SetTitleSize(0.05)
-    referenceHist.GetYaxis().SetTitleOffset(1.)
-    referenceHist.GetYaxis().CenterTitle()
-    referenceHist.SetStats( False)
-    referenceHist.GetXaxis().SetTitle("m_{34} [GeV]")
+    return uncorrectedHist, ZConstrFSRHist, uncorrectedHist_m4lAll, ZConstrFSRHist_m4lAll
 
 
+def prepHistOptics(hist):
 
-    m34_FSR_Constrained.SetMarkerStyle(5)
-    #m34_FSR_Constrained.SetMarkerSize(1)
-    m34_FSR_Constrained.SetMarkerColor(2)
-    m34_FSR_Constrained.SetStats( False)
+    if isinstance(hist,list): 
+        for element in hist: prepHistOptics(element)
+        return None
 
-    canvas = ROOT.TCanvas("canv","canv",720,720)
+    #uncorrectedHist.SetFillStyle(3244)
+    hist.SetMarkerColor(1)
+    hist.GetYaxis().SetTitle("Events / " + str(hist.GetBinWidth(1) )+" GeV" )
+    hist.GetYaxis().SetTitleSize(0.05)
+    hist.GetYaxis().SetTitleOffset(1.)
+    hist.GetYaxis().CenterTitle()
+    hist.SetStats( False)
+    hist.GetXaxis().SetTitle("m_{34} [GeV]")
+
+    return None
+
+
+def prepRatioHistOptics(ratioHist):
+
+    ratioHist.SetMarkerColor(1)
+
+    maxRatioVal , _ = histHelper.getMaxBin(ratioHist , useError = False, skipZeroBins = True)
+    minRatioVal , _ = histHelper.getMinBin(ratioHist , useError = False, skipZeroBins = True)
+
+    ratioHist.GetYaxis().SetRangeUser(minRatioVal * 0.99, maxRatioVal * 1.01)
+
+    ratioHist.SetTitle("")
+    
+    ratioHist.GetYaxis().SetNdivisions( 506, True)  # XYY x minor divisions YY major ones, optimizing around these values = TRUE
+    ratioHist.GetYaxis().SetLabelSize(0.1)
+
+    ratioHist.GetYaxis().SetTitle("FSR_ZConstrained / reference ")
+    ratioHist.GetYaxis().SetTitleSize(0.08)
+    ratioHist.GetYaxis().SetTitleOffset(0.6)
+    ratioHist.GetYaxis().CenterTitle()
+
+    ratioHist.SetMarkerStyle(8)
+    ratioHist.SetStats( False)
+
+    ratioHist.GetXaxis().SetLabelSize(0.12)
+    ratioHist.GetXaxis().SetTitleSize(0.13)
+    ratioHist.GetXaxis().SetTitleOffset(1.0)
+    ratioHist.GetXaxis().SetTitle("m_{34} [GeV]")
+
+    return None
+
+
+def makeCanvasWithHistograms(uncorrectedHist, ZConstrFSRHist, canvasName = "canv"):
+
+    canvas = ROOT.TCanvas(canvasName, canvasName,720,720)
 
     histPadYStart = 3.5/13
     histPad = ROOT.TPad("histPad", "histPad", 0, histPadYStart, 1, 1);
     histPad.Draw();              # Draw the upper pad: pad1
     histPad.cd();                # pad1 becomes the current pad
 
-    referenceHist.SetStats( False)
-    referenceHist.Draw("")
-    m34_FSR_Constrained.Draw("same P")
+    uncorrectedHist.Draw("")
+    ZConstrFSRHist.Draw("same P")
 
 
     legend = setupTLegend()
-    legend.AddEntry(referenceHist , "reference" , "l");
-    legend.AddEntry(m34_FSR_Constrained , "FSR recovered and Z constrained" , "p");
+    legend.AddEntry(uncorrectedHist , "uncorrected m34" , "l");
+    legend.AddEntry(ZConstrFSRHist , "FSR recovered and Z constrained" , "p");
     legend.Draw()
 
     canvas.cd()
@@ -94,37 +130,47 @@ if __name__ == '__main__':
     ratioPad.cd();                # pad1 becomes the current pad
 
 
-    ratioHist = m34_FSR_Constrained.Clone( m34_FSR_Constrained.GetName()+"_Clone" )
-    ratioHist.Divide(referenceHist)
+    ratioHist = ZConstrFSRHist.Clone( ZConstrFSRHist.GetName()+"_Clone" )
+    ratioHist.Divide(uncorrectedHist)
 
-    ratioHist.SetMarkerColor(1)
+    prepRatioHistOptics(ratioHist)
 
-    maxRatioVal , _ = histHelper.getMaxBin(ratioHist , useError = False, skipZeroBins = True)
-    minRatioVal , _ = histHelper.getMinBin(ratioHist , useError = False, skipZeroBins = True)
-
-    ratioHist.GetYaxis().SetRangeUser(minRatioVal * 0.99, maxRatioVal * 1.01)
-
-
-    ratioHist.SetTitle("")
-    
-    ratioHist.GetYaxis().SetNdivisions( 506, True)  # XYY x minor divisions YY major ones, optimizing around these values = TRUE
-    ratioHist.GetYaxis().SetLabelSize(0.1)
-
-    ratioHist.GetYaxis().SetTitle("FSR_ZConstrained / reference ")
-    ratioHist.GetYaxis().SetTitleSize(0.08)
-    ratioHist.GetYaxis().SetTitleOffset(0.6)
-    ratioHist.GetYaxis().CenterTitle()
-
-
-    ratioHist.SetMarkerStyle(8)
-    ratioHist.SetStats( False)
     ratioHist.Draw("P")
 
-    ratioHist.GetXaxis().SetLabelSize(0.12)
-    ratioHist.GetXaxis().SetTitleSize(0.13)
-    ratioHist.GetXaxis().SetTitleOffset(1.0)
-    ratioHist.GetXaxis().SetTitle("m_{34} [GeV]")
-
     canvas.Update()
+
+    outDict = { "canvas" : canvas, "histPad" : histPad, "ratioPad" : ratioPad, "ratioHist" : ratioHist, "legend" : legend  }
+
+    return outDict
+
+
+if __name__ == '__main__':
+
+    #miniTreeFile = ROOT.TFile("data15to16_13TeV.root","OPEN")
+    miniTreeFile = ROOT.TFile("mc16_13TeV.345706.Sherpa_222_NNPDF30NNLO_ggllll_130M4l.root","OPEN")
+
+    miniTree = miniTreeFile.Get("tree_incl_all")
+
+    cutOn = "m4l_constrained >115 && m4l_constrained<130"
+
+
+    uncorrectedHist, ZConstrFSRHist, uncorrectedHist_m4lAll, ZConstrFSRHist_m4lAll = prepHistograms(miniTreeFile)
+
+
+    fillMZ2HistPair(miniTree, uncorrectedHist       , ZConstrFSRHist       , cutOn = cutOn)
+    fillMZ2HistPair(miniTree, uncorrectedHist_m4lAll, ZConstrFSRHist_m4lAll, cutOn = "")
+
+    DSID = getDSIDStr( miniTreeFile.GetName() )
+    #uncorrectedHist.SetTitle("full m4l range")
+    uncorrectedHist.SetTitle("m4l in Higgs Window, sample # "+ DSID )
+
+    prepHistOptics([uncorrectedHist, ZConstrFSRHist])
+
+    ZConstrFSRHist.SetMarkerStyle(5)
+    ZConstrFSRHist.SetMarkerColor(2)
+
+    outDict = makeCanvasWithHistograms(uncorrectedHist, ZConstrFSRHist, canvasName = "canv_"+DSID)
+
+
 
     import pdb; pdb.set_trace() # import the debugger and instruct it to stop here
