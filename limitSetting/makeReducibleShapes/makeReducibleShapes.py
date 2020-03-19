46e0d93e3ad0685be6cb8806aa6a9504bc9e974f
Christian Weber <ChristianTMWeber@gmal.com>
2020-03-19 19:49:53 -0400

    Work on makind reducible shape estimates

--- limitSetting/makeReducibleShapes/makeReducibleShapes.py
+++ limitSetting/makeReducibleShapes/makeReducibleShapes.py
@@ -1,156 +1,126 @@
-import ROOT # to do all the ROOT stuff
+import ROOT
 
-import argparse # to parse command line options
-import time # for measuring execution time
-import re
+import math
 
-# import sys and os.path to be able to import functions from parent directory (and parentparent directory)
-import sys 
-from os import path
-sys.path.append( path.dirname( path.dirname( path.dirname( path.abspath(__file__) ) ) ) ) # need to append the parent directory here explicitly to be able to import plotPostProcess
-
-import functions.rootDictAndTDirTools as rootDictAndTDirTools
-from plotPostProcess import DSIDHelper
+import copy
 
 
+def getRooRealVarFromTree( branchName, relevantTTree ):
+    return ROOT.RooRealVar(branchName,branchName, math.floor(relevantTTree.GetMinimum(branchName)), math.ceil(relevantTTree.GetMaximum(branchName)))
 
-sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) )  ) # add also the direct parent directory
-import limitFunctions.makeHistDict as makeHistDict
-from limitFunctions.visualizeSignalOverview import getMasspointDict
-import limitFunctions.RooIntegralMorphWrapper as integralMorphWrapper
-import limitFunctions.reportMemUsage as reportMemUsage
+
+def getZXTTree( fileName, treeName = "t_ZXTree"):
+    aTFile = ROOT.TFile(fileName, "OPEN")
+    aTTree = aTFile.Get(treeName)
+
+    ROOT.SetOwnership(aTFile, False)  # we change the ownership here to False in the attempt to prevent deletion
+    ROOT.SetOwnership(aTTree, False)  # we change the ownership here to False in the attempt to prevent deletion
+
+    return aTTree
 
 
 
+def getShapeRooDataSetAndIndepVar(ZXTTree):
+
+    ### Setup variables for RooDataSet etc.
+
+    m34 = getRooRealVarFromTree( "llll_m34", aTTree )
+    #m34.setMax(45000)
+    m4l = getRooRealVarFromTree( "llll_m4l", aTTree )
+
+    weight = getRooRealVarFromTree( "weight", aTTree )
+    weight.setMin(0)
+
+    decayChannel = getRooRealVarFromTree( "decayChannel", aTTree )
+
+    # and put the into sets and lists, as needed by the different Root Methods beliw
+    anArgSet = ROOT.RooArgSet(m34, weight, m4l, decayChannel)
+    anArgList = ROOT.RooArgList(anArgSet)
+
+    # decay channel 1 <-> 4mu
+    #               2 <-> 2e2mu
+    #               3 <-> 2mu2e
+    #               4 <-> 4e
+
+    if   finalState == "llmumu": decaySetting = "decayChannel <= 2"
+    elif finalState == "llee"  : decaySetting = "decayChannel >= 3"
+
+    aFormula = ROOT.RooFormulaVar("cuts"," llll_m4l > 115000 && llll_m4l < 130000 &&  " + decaySetting, anArgList) #  &&       
 
 
-def setupTLegend():
-    # set up a TLegend, still need to add the different entries
-    TLegend = ROOT.TLegend(0.10,0.70,0.60,0.90)
-    TLegend.SetFillColor(ROOT.kWhite)
-    TLegend.SetLineColor(ROOT.kWhite)
-    TLegend.SetNColumns(1);
-    TLegend.SetFillStyle(0);  # make legend background transparent
-    TLegend.SetBorderSize(0); # and remove its border without a border
+        #aTTree.GetMinimum("mc_channel_number") aTTree.GetMaximum("mc_channel_number")
 
-    return TLegend
+    if aTTree.GetMaximum("mc_channel_number") < 1.: 
+        print("Data Sample detected, omitting weights")
+        aDataSet   = ROOT.RooDataSet("h4lData", "h4lData", aTTree, anArgSet, aFormula)
+    else: aDataSet = ROOT.RooDataSet("h4lData", "h4lData", aTTree, anArgSet, aFormula, "weight")
 
 
-
-def addllHistsToMasterHistDict(masterHistDict):
-
-    lowHist  = masterHistDict[channel][ masspointDict[lowMass]  ][systematic][flavor]
-
-    for channel in masterHistDict.keys():
-        for eventType in masterHistDict[channel].keys():
-            for systematic in masterHistDict[channel][eventType].keys(): 
-                for kinematic in masterHistDict[channel][eventType][systematic].keys(): kinematic
-                
-
-    return None
-
-
-
-def makellCopyOfHist(aHist):
-
-    def makeRenamedHistCopy(originalHist, regexPattern, replaceStr):
-
-        newName = re.sub(regexPattern, replaceStr, originalHist.GetName() )
-        newTitle = re.sub(regexPattern, replaceStr, originalHist.GetTitle() )
-
-        newHist = originalHist.Clone(newName)
-        newHist.SetTitle(newTitle)
-
-        return newHist
-
-    llmumuSignifiers = "(2e2mu)|(2e2m)|(4mu)"
-    lleeSignifiers = "(2mu2e)|(2m2e)|(4e)"
-
-    if    re.search( llmumuSignifiers, aHist.GetName()): llHist = makeRenamedHistCopy(aHist, llmumuSignifiers, "llmumu")
-    elif  re.search( lleeSignifiers, aHist.GetName()):   llHist = makeRenamedHistCopy(aHist, lleeSignifiers, "llee")
-    else: return None
-
-
-    return llHist
-
-
-
+    # m34 is here the independent variable
+    return aDataSet, m34
 
 
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser()
+    ## llmumu shapes are from Z+HeavyFlavor and ttbar MC 
+    ## llee   shapes are from Z+HeavyFlavor MC and 3l+X 
 
-    parser.add_argument("--input", "-i", type=str, default="post_20200219_204021__ZX_Run2_AllReducibles_May_slimmed.root",
-        help="name or path to the input files")
+    #fileName = "post_20200228_152824__ZX_Run2_ttbar_May_Minitree.root"
 
-    args = parser.parse_args()
-
-    ######################################################
-    # Open the attached .root file and loop over all elements over it
-    ######################################################
-    startTime = time.time()
-    postProcessedData = ROOT.TFile(args.input,"READ"); # open the file with te data from the ZdZdPostProcessing
-
-    ######################################################
-    # Set up DSID helper
-    ######################################################
-    # the DSID helper has two main functions
-    # 1) administrating the metadata 
-    #    i.e. parsing the meta data files and based on them providing a scaling for the MC samples
-    # 2) grouping DSIDs into physics categories for the plots
-    #    e.g. grouping DSIDs 345060 and 341488 (among others) into one histogram for the "H->ZZ*->4l" process
-    myDSIDHelper = DSIDHelper()
-    myDSIDHelper.importMetaData("../../metadata/md_bkg_datasets_mc16e_All.txt") # since the DSID helper administrates the meta data for the MC samples we must provide it with the meta data locati
-    myDSIDHelper.setMappingOfChoice( "analysisMapping" )
-
-    myDSIDHelper.fillSumOfEventWeightsDict(postProcessedData)
-
-
-    # sort hists into a convinient Dict
-
-    nRelevantHistsProcessed = 0
-
-    for path, myTObject  in rootDictAndTDirTools.generateTDirPathAndContentsRecursive(postProcessedData, newOwnership = None):  
-        # set newOwnership to 'None' here and let root handle the ownership itself for now, 
-        # otherwise we are getting a segmentation fault?!
-
-        if makeHistDict.skipTObject(path, myTObject): continue # skip non-relevant histograms
-
-        if "Nominal" not in path: continue
-
-        DSID = myDSIDHelper.idDSID( path)
-
-
-
-        if int(DSID) not in myDSIDHelper.analysisMapping["Reducible"]: continue
-
-        vetoRegexStr= "(BVeto)|(WZ)" 
-        if re.search( vetoRegexStr, myDSIDHelper.physicsSubProcessByDSID[int(DSID)]): continue
-
-        # make a copy of the histogram, where (when appropriate) the flavor composition has been collapsed to llmumu or llee
-        # do this so we can make llmumu or llee plots eventually
-        lleeOrllmumuHist = makellCopyOfHist(myTObject) # do this before entering myTObject into the makeHistDict.fillHistDict(...), because the latter modifies the myTObject
-
-        masterHistDict = makeHistDict.fillHistDict(path, myTObject , "mc16ade", myDSIDHelper ) 
-      
-        if lleeOrllmumuHist: masterHistDict = makeHistDict.fillHistDict(path, lleeOrllmumuHist , "mc16ade", myDSIDHelper ) 
-
-        nRelevantHistsProcessed += 1
-
-        if nRelevantHistsProcessed %100 == 0:  print( path, myTObject)
-        #if args.quick and (nRelevantHistsProcessed == 2000): break
-
-
-    #masterHistDict['signalRegion']['Reducible']['Nominal']["llmumu"].Integral()
-
+    shapeSourceFiles = { "HeavyFlavor" : "post_20200228_203930__ZX_Run2_ZJetBFilter_May_Minitree.root", 
+                         "ttBar"       : "post_20200228_152824__ZX_Run2_ttbar_May_Minitree.root",
+                         "3l+X"        : "post_20200319_122149__ZX_Run2_Data_May_3lX_Minitree.root"      }
     
+
+    canvasDict = {}
+
+    for finalState in ["llmumu" , "llee"]:
+
+        canvasCounter = 0
+        canv = ROOT.TCanvas( finalState+"_canvas", finalState+"_canvas", 900, 900)
+        canv.Divide(2,2)
+        canvasDict[finalState] = canv
+
+        for shapeType in shapeSourceFiles:
+
+            fileName = shapeSourceFiles[shapeType]
+
+
+
+            aTTree = getZXTTree( fileName )
+
+
+            aDataSet, m34 = getShapeRooDataSetAndIndepVar(aTTree)
+
+            kest1 = ROOT.RooKeysPdf("kest1", "kest1", m34, aDataSet)#                        ROOT.RooKeysPdf.MirrorBoth)
+            kest2 = ROOT.RooKeysPdf("kest2", "kest2", m34, aDataSet)#                        ROOT.RooKeysPdf.MirrorBoth)
+            kest3 = ROOT.RooKeysPdf("kest3", "kest3", m34, aDataSet, ROOT.RooKeysPdf.MirrorBoth)#                        ROOT.RooKeysPdf.MirrorBoth)
+
+            plotName = shapeType + ": " + finalState
+
+            canvasCounter += 1
+            canv.cd(canvasCounter)
+
+            xFrameHists = m34.frame() # frame to plot my PDFs on
+            xFrameHists.SetTitle(plotName)
+            aDataSet.plotOn(xFrameHists)
+            #aDataSetWeighted.plotOn(xFrameHists, ROOT.RooFit.LineColor(ROOT.kGreen+1),ROOT.RooFit.MarkerColor(ROOT.kGreen+1) )#, ROOT.RooFit.MarkerColor(ROOT.kGreen+1))
+            #
+            #kest1.plotOn(xFrameHists)
+            kest2.plotOn(xFrameHists,ROOT.RooFit.LineColor(ROOT.kRed+1)) # , ROOT.RooFit.LineWidth(5) 
+            kest3.plotOn(xFrameHists,ROOT.RooFit.LineColor(ROOT.kGreen+1),ROOT.RooFit.LineStyle(7))
+
+
+            xFrameHists.Draw()
+            canv.Update()
 
     import pdb; pdb.set_trace() # import the debugger and instruct it to stop here
 
 
+    # aTFile.Get()
 
+    # aDataSetWeighted.sumEntries()
+    # aTFile.Get("410472").Get("Nominal").Get("h_ZXSR_All_HWindow_m34").Integral()
 
-
-    #import pdb; pdb.set_trace() # import the debugger and instruct it to stop here
+    import pdb; pdb.set_trace() # import the debugger and instruct it to stop here
