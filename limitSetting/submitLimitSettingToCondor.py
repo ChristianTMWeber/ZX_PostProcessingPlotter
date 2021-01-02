3046341051f6e44ea462d7167f9def9e5beb2e04
Christian Weber <chweber@spar0105.usatlas.bnl.gov>
2021-01-02 13:23:42 -0500

    add options for asymptotic limits, prePostfits, p0 scan, and different flavors

--- limitSetting/submitLimitSettingToCondor.py
+++ limitSetting/submitLimitSettingToCondor.py
@@ -2,6 +2,8 @@ import re
 import os
 import time 
 
+import ROOT # not needed here, but better to throw an error here, then after submission
+
 def makeSubmitScript( shellScriptName):
 
     submitScriptName = re.sub(".sh", ".sub", shellScriptName) # replace '.sh' with '.sub'
@@ -29,30 +31,62 @@ def makeSubmitScript( shellScriptName):
 
 if __name__ == '__main__':
 
-    parentFolder = "/usatlas/u/chweber/usatlasdata/backgroundDAODs/"
+    massPoints = range(15,56,1)
+    #massPoints = [26, 30, 32, 54, 55 ]
 
-    massPoints = range(15,56,5)
-    #massPoints = [15]
+    #submissionType = "asymptoticLimits"
+    submissionType = "prePostFitImpacts"
+    #submissionType = "nullHypothesisPValue"
 
-    flavor = "All" # "2mu2e", "2l2e"
+
+    flavor = "All" 
+    #flavor = "2l2mu"
+    #flavor = "2l2e"
+
+
+    # 
+
 
     inputFileName = "post_20200915_171012_ZX_Run2_BckgSignal_PreppedHist_UnblindedData_V5.root"
-    outputDir = "asymptoticLimits_"+flavor+"_"+re.sub(".root", "", inputFileName)
+    #
+
+    extraOptions =""
+
+    if submissionType == "asymptoticLimits":
+
+        outputDir = "asymptoticLimits_"+flavor+"_"+re.sub(".root", "", inputFileName)
+        limitTypeOption = "asymptotic"
+
+    elif submissionType == "prePostFitImpacts":
+
+        outputDir = "prePostFitImpacts_"+flavor+"_"+re.sub(".root", "", inputFileName)
+        extraOptions = " --doPrePostFitImpacts"
+
+        limitTypeOption = "observed"
+
+    elif submissionType == "nullHypothesisPValue":
+
+        outputDir = "pValues_"+flavor+"_"+re.sub(".root", "", inputFileName)
+        limitTypeOption = "p0Calculation"
+
+
+
 
 
 
     for mass in massPoints:
 
-        outputFileName = "limitV5_PGM_%s_Zd%i.root" %(flavor,mass)
+        outputFileName = "limitV5_PMG_%s_Zd%i.root" %(flavor,mass)
 
 
-        submitLine = "python limitSetting.py --inputFileName  %s  --limitType asymptotic --nSystematics 0  --outputDir %s --outputFileName %s --flavor %s --nMassPoints %i" %(inputFileName, outputDir, outputFileName , flavor, mass)
-        #submitLine = "python limitSetting.py --inputFileName  post_20200809_203927_ZX_Run2_BckgSignal_PreppedHist_PMGWeights_V3.root  --limitType asymptotic --nSystematics -1  --outputDir /direct/usatlas+u/chweber/ZdZd_area/ZX_PostProcessingPlotter/limitSetting/asymptotiveLimitV3b_noPMGweights --outputFileName limitV3b_noPGM_Zd%i.root --nMassPoints %i" %(mass, mass)
-        #submitLine = "python limitSetting.py --inputFileName  post_20200809_203927_ZX_Run2_BckgSignal_PreppedHist_PMGWeights_V3.root  --limitType asymptotic --nSystematics 0  --skipStatAndTheoryError  --outputDir /direct/usatlas+u/chweber/ZdZd_area/ZX_PostProcessingPlotter/limitSetting/testCondor --outputFileName test_Zd_%i.root --nMassPoints %i" %(mass, mass)
+        submitLine = "python limitSetting.py --inputFileName  %s  --limitType %s --nSystematics -1 --dataToOperateOn data  --outputDir %s --outputFileName %s --flavor %s --nMassPoints %i  %s" %(inputFileName, limitTypeOption,  outputDir, outputFileName , flavor, mass, extraOptions)
+        #submitLine = "python limitSetting.py --inputFileName  %s  --limitType observed --nSystematics -1   --dataToOperateOn data  --outputDir %s --outputFileName %s --flavor %s --nMassPoints %i " %(inputFileName, outputDir, outputFileName , flavor, mass)
 
 
         #import pdb; pdb.set_trace()
-        shellScript = "batchRunV5_%s_%i.sh" % (flavor,mass) 
+        shellScript = "batchRunV5_%s_%i_%s.sh" % (flavor,mass, submissionType) 
+        #shellScript = "batchRunV5_%s_%i_PrePostFit.sh" % (flavor,mass) 
+
         with open(shellScript, "w") as shellWrite:
             shellWrite.write('#!/bin/bash\n')
             shellWrite.write('%s\n' %submitLine)
