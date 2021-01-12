af67aa9b9fe602da1e0fff43f3d1da6b95e3d944
Christian Weber <ChristianTMWeber@gmal.com>
2021-01-12 17:20:39 -0500

    Update to convertGridToyResults.py to loop over multiple files

--- limitSetting/convertGridToyResults.py
+++ limitSetting/convertGridToyResults.py
@@ -28,84 +28,89 @@ if __name__ == '__main__':
     #skimmedToyResultsPath = "mc16adeToyResultsV5.23"
     #skimmedToyResultsPath = "mc16adeToyResultsV5.24"
     #skimmedToyResultsPath = "mc16adeToyResultsV5.25"
-    skimmedToyResultsPath = "mc16adeToyResultsV6.30"
-
-    
-
-    outputFileName = skimmedToyResultsPath +".root"
-
-
-    limitType = "gridToys"
-
-    skimmedToyResults = os.listdir(skimmedToyResultsPath)
+    #skimmedToyResultsPath = "mc16adeToyResultsV6.30"
+    #skimmedToyResultsPath = "mc16adeToyResultsV5.31"
+    #skimmedToyResultsPath = "mc16adeToyResultsV6.32"
+    #skimmedToyResultsPath = "mc16adeToyResultsV6.33"
+    #skimmedToyResultsPath = "mc16adeToyResultsV6.34"
 
 
-    bestEstimateDict   = collections.defaultdict(list)
-    upperLimits1SigDict = collections.defaultdict(list)
-    upperLimits2SigDict = collections.defaultdict(list)
-    lowLimits1SigDict = collections.defaultdict(list)
-    lowLimits2SigDict = collections.defaultdict(list)
+    for skimmedToyResultsPath in ["mc16adeToyResultsV6.32", "mc16adeToyResultsV6.33", "mc16adeToyResultsV6.34"]:
+
+        outputFileName = skimmedToyResultsPath +".root"
 
 
-    observedLimitGraph    = graphHelper.createNamedTGraphAsymmErrors("observedLimitGraph")
-    expectedLimitsGraph_1Sigma = graphHelper.createNamedTGraphAsymmErrors("expectedLimits_1Sigma")
-    expectedLimitsGraph_2Sigma = graphHelper.createNamedTGraphAsymmErrors("expectedLimits_2Sigma")
+        limitType = "gridToys"
+
+        skimmedToyResults = os.listdir(skimmedToyResultsPath)
 
 
-    for toyResultFile in  skimmedToyResults:
-
-        mass = re.search("\d\d",toyResultFile).group()
-        massPoint = int(mass)
-
-        toyResultTFile = ROOT.TFile(os.path.join(skimmedToyResultsPath,toyResultFile),"OPEN")
-
-        hypoTestInvResult = toyResultTFile.Get("result_mu")
-
-        #import pdb; pdb.set_trace() # import the debugger and instruct it to stop here
-
-        likelihoodLimit_observed      = limitSetting.translateLimits( hypoTestInvResult, nSigmas = 1 ,getObservedAsymptotic = True)
-
-        likelihoodLimit_1sig      = limitSetting.translateLimits( hypoTestInvResult, nSigmas = 1 )
-        likelihoodLimit_2Sig = limitSetting.translateLimits( hypoTestInvResult, nSigmas = 2 )
-
-        print(mass)
-        likelihoodLimit_observed.Print()
+        bestEstimateDict   = collections.defaultdict(list)
+        upperLimits1SigDict = collections.defaultdict(list)
+        upperLimits2SigDict = collections.defaultdict(list)
+        lowLimits1SigDict = collections.defaultdict(list)
+        lowLimits2SigDict = collections.defaultdict(list)
 
 
-        bestEstimateDict[mass].append( likelihoodLimit_observed.getVal() )
-        upperLimits1SigDict[mass].append(likelihoodLimit_1sig.getMax())
-        upperLimits2SigDict[mass].append(likelihoodLimit_2Sig.getMax())
-        lowLimits1SigDict[mass].append(likelihoodLimit_1sig.getMin())
-        lowLimits2SigDict[mass].append(likelihoodLimit_2Sig.getMin())
+        observedLimitGraph    = graphHelper.createNamedTGraphAsymmErrors("observedLimitGraph")
+        expectedLimitsGraph_1Sigma = graphHelper.createNamedTGraphAsymmErrors("expectedLimits_1Sigma")
+        expectedLimitsGraph_2Sigma = graphHelper.createNamedTGraphAsymmErrors("expectedLimits_2Sigma")
 
 
-        graphHelper.fillTGraphWithRooRealVar(observedLimitGraph, massPoint, likelihoodLimit_observed)
-        graphHelper.fillTGraphWithRooRealVar(expectedLimitsGraph_1Sigma, massPoint, likelihoodLimit_1sig)
-        graphHelper.fillTGraphWithRooRealVar(expectedLimitsGraph_2Sigma, massPoint, likelihoodLimit_2Sig)
+        for toyResultFile in  skimmedToyResults:
+
+            mass = re.search("\d\d",toyResultFile).group()
+            massPoint = int(mass)
+
+            toyResultTFile = ROOT.TFile(os.path.join(skimmedToyResultsPath,toyResultFile),"OPEN")
+
+            hypoTestInvResult = toyResultTFile.Get("result_mu")
+
+            #import pdb; pdb.set_trace() # import the debugger and instruct it to stop here
+
+            likelihoodLimit_observed      = limitSetting.translateLimits( hypoTestInvResult, nSigmas = 1 ,getObservedAsymptotic = True)
+
+            likelihoodLimit_1sig      = limitSetting.translateLimits( hypoTestInvResult, nSigmas = 1 )
+            likelihoodLimit_2Sig = limitSetting.translateLimits( hypoTestInvResult, nSigmas = 2 )
+
+            print(mass)
+            likelihoodLimit_observed.Print()
+
+
+            bestEstimateDict[mass].append( likelihoodLimit_observed.getVal() )
+            upperLimits1SigDict[mass].append(likelihoodLimit_1sig.getMax())
+            upperLimits2SigDict[mass].append(likelihoodLimit_2Sig.getMax())
+            lowLimits1SigDict[mass].append(likelihoodLimit_1sig.getMin())
+            lowLimits2SigDict[mass].append(likelihoodLimit_2Sig.getMin())
+
+
+            graphHelper.fillTGraphWithRooRealVar(observedLimitGraph, massPoint, likelihoodLimit_observed)
+            graphHelper.fillTGraphWithRooRealVar(expectedLimitsGraph_1Sigma, massPoint, likelihoodLimit_1sig)
+            graphHelper.fillTGraphWithRooRealVar(expectedLimitsGraph_2Sigma, massPoint, likelihoodLimit_2Sig)
 
 
 
 
 
 
-    writeTFile = ROOT.TFile( outputFileName,  "RECREATE")# "UPDATE")
-    writeTFile.cd()
-    bestEstimatesTTree   = fillTTreeWithDictOfList(bestEstimateDict, treeName = "bestEstimates_"+limitType)
-    upperLimits1SigTTree = fillTTreeWithDictOfList(upperLimits1SigDict, treeName = "upperLimits1Sig_"+limitType)
-    upperLimits2SigTTree = fillTTreeWithDictOfList(upperLimits2SigDict, treeName = "upperLimits2Sig_"+limitType)
+        writeTFile = ROOT.TFile( outputFileName,  "RECREATE")# "UPDATE")
+        writeTFile.cd()
+        bestEstimatesTTree   = fillTTreeWithDictOfList(bestEstimateDict, treeName = "bestEstimates_"+limitType)
+        upperLimits1SigTTree = fillTTreeWithDictOfList(upperLimits1SigDict, treeName = "upperLimits1Sig_"+limitType)
+        upperLimits2SigTTree = fillTTreeWithDictOfList(upperLimits2SigDict, treeName = "upperLimits2Sig_"+limitType)
 
-    lowLimits1SigTTree = fillTTreeWithDictOfList(lowLimits1SigDict, treeName = "lowLimits1Sig_"+limitType)
-    lowLimits2SigTTree = fillTTreeWithDictOfList(lowLimits2SigDict, treeName = "lowLimits2Sig_"+limitType)
+        lowLimits1SigTTree = fillTTreeWithDictOfList(lowLimits1SigDict, treeName = "lowLimits1Sig_"+limitType)
+        lowLimits2SigTTree = fillTTreeWithDictOfList(lowLimits2SigDict, treeName = "lowLimits2Sig_"+limitType)
 
 
-    observedLimitGraph.Write()
-    expectedLimitsGraph_1Sigma.Write()
-    expectedLimitsGraph_2Sigma.Write()
+        observedLimitGraph.Write()
+        expectedLimitsGraph_1Sigma.Write()
+        expectedLimitsGraph_2Sigma.Write()
 
-    
-    writeTFile.Write()
+        
+        writeTFile.Write()
 
-    writeTFile.Close()
+        writeTFile.Close()
 
 
 
@@ -115,5 +120,5 @@ if __name__ == '__main__':
 
 
     print("All Done!")
-    import pdb; pdb.set_trace() # import the debugger and instruct it to stop here
+    #import pdb; pdb.set_trace() # import the debugger and instruct it to stop here
 
